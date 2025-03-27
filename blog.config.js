f844ea8c01f078b46d04aa64a22ce38089200eed
drhuangnz <erichuangnz@gmail.com>
2025-03-28 12:01:44 +1300

    Update blog.config.js

--- blog.config.js
+++ blog.config.js
@@ -15,7 +15,7 @@ const BLOG = {
   APPEARANCE_DARK_TIME: process.env.NEXT_PUBLIC_APPEARANCE_DARK_TIME || [18, 6], // 夜间模式起至时间，false时关闭根据时间自动切换夜间模式
 
   AUTHOR: process.env.NEXT_PUBLIC_AUTHOR || 'Dr Huang', // 您的昵称 例如 tangly1024
-  BIO: process.env.NEXT_PUBLIC_BIO || 'Data Scientist📊| Data🧬| DIY Enthusiast🔬', // 作者简介
+  BIO: process.env.NEXT_PUBLIC_BIO || 'Data Scientist📊| Dad🧬| DIY Enthusiast🔬', // 作者简介
   LINK: process.env.NEXT_PUBLIC_LINK || 'https://drhuang.top', // 网站地址
   KEYWORDS: process.env.NEXT_PUBLIC_KEYWORD || 'Notion, 博客，创业，DIY,博士，STEM,教育', // 网站关键词 英文逗号隔开
   BLOG_FAVICON: process.env.NEXT_PUBLIC_FAVICON || '/favicon.ico', // blog favicon 配置, 默认使用 /public/favicon.ico，支持在线图片，如 https://img.imesong.com/favicon.png
